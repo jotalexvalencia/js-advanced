a2f10edd2db0b0ff40d67bd00ffae4ccd6f51436
jotalexvalencia <jorgealexandervalencia@gmail.com>
2025-01-30 14:55:31 -0500

    generator function other example

--- src/concepts/09-generators.js
+++ src/concepts/09-generators.js
@@ -4,13 +4,30 @@
  * @param {HTMLDivElement} element 
  */
 export const generatorFunctionsComponent = (element) => {
-  const myGenerator = myFirstGeneratorFunction();
-  console.log(myGenerator.next());
-  console.log(myGenerator.next());
-  console.log(myGenerator.next());
-  console.log(myGenerator.next());
-  console.log(myGenerator.next());
+  // const myGenerator = myFirstGeneratorFunction();
+  // console.log(myGenerator.next());
+
+  const genId = idGenerator();
+
+  const button = document.createElement('button');
+  button.innerText = 'Click me';
+  element.append(button);
+
+  const renderButton = () => {
+    const { value } = genId.next();
+    button.innerText = `Click ${value}`;
+  }
+
+  button.addEventListener('click', renderButton);
 };
+
+function* idGenerator() {
+  let currentId = 0;
+
+  while (true) {
+    yield ++currentId;
+  }
+}
 
 function* myFirstGeneratorFunction() {
   yield 'Primer valor';
